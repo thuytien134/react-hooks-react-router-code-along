a46ff85193554e3b88b0178e3f6f5c12b87b86ac
thuythuytiennguyen <thuytienthuytien134@gmail.com>
2022-06-10 10:06:53 -0700

    completed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,100 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
+// function Home() {
+//   return (
+//     <div>
+//       <h1>Home!</h1>
+//     </div>
+//   );
+// }
 
-function App() {
-  return <Home />;
-}
+// function About() {
+//   return (
+//     <div>
+//       <h1>This is my about component!</h1>
+//     </div>
+//   );
+// }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// function Login() {
+//   return (
+//     <div>
+//       <h1>Login</h1>
+//       <form>
+//         <div>
+//           <input type="text" name="username" placeholder="Username" />
+//         </div>
+//         <div>
+//           <input type="password" name="password" placeholder="Password" />
+//         </div>
+//         <input type="submit" value="Submit" />
+//       </form>
+//     </div>
+//   );
+// }
+
+// /* Add basic styling for NavLinks */
+// const linkStyles = {
+//   display: "inline-block",
+//   width: "50px",
+//   padding: "12px",
+//   margin: "0 6px 6px",
+//   background: "blue",
+//   textDecoration: "none",
+//   color: "white",
+// };
+
+
+// /* define the NavBar component */
+// function NavBar() {
+//   return (
+//     <div>
+//       <NavLink
+//         to="/"
+//         /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+//         exact
+//         /* add styling to Navlink */
+//         style={linkStyles}
+//         /* add prop for activeStyle */
+//         activeStyle={{
+//           background: "darkblue",
+//         }}
+//       >
+//         Home
+//       </NavLink>
+//       <NavLink
+//         to="/about"
+//         exact
+//         style={linkStyles}
+//         activeStyle={{
+//           background: "darkblue",
+//         }}
+//       >
+//         About
+//       </NavLink>
+//       <NavLink
+//         to="/login"
+//         exact
+//         style={linkStyles}
+//         activeStyle={{
+//           background: "darkblue",
+//         }}
+//       >
+//         Login
+//       </NavLink>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return <Home />;
+// }
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  , document.getElementById("root"));
